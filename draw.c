b5ec8eeef5ff1fbf649eaad5776ff484001ab60e
Oskar Banshee <obanshee@ga-s6.21-school.ru>
2019-11-12 19:44:44 +0300

    draw.c

--- draw.c
+++ draw.c
@@ -6,7 +6,7 @@
 /*   By: obanshee <obanshee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/11 21:05:32 by obanshee          #+#    #+#             */
-/*   Updated: 2019/11/12 15:24:09 by obanshee         ###   ########.fr       */
+/*   Updated: 2019/11/12 19:43:56 by obanshee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ void	draw_line(t_param *param, t_vector point1, t_vector point2, t_vector *first
 	e = dy - dx;
 	i = point1.x;
 	j = point1.y;
-	while (i < point2.x - 1)
+	while (i <= point2.x - 1)
 	{
 		draw_pixel(param, i, j, first_point);
 		if (e >= 0)
@@ -45,6 +45,76 @@ void	draw_line(t_param *param, t_vector point1, t_vector point2, t_vector *first
 		}
 		i++;
 		e += dy;
+	}
+}
+
+void	draw_line_vert(t_param *param, t_vector point1, t_vector point2, t_vector *first_point)
+{
+	int	dx;
+	int	dy;
+//	int	e;
+	int	i;
+	int	j;
+
+//	if (not point 1 and 2 on window)
+//		return ;
+	dx = point2.x - point1.x;
+	dy = point2.y - point1.y;
+	i = point1.x;
+	j = point1.y;
+	while (j <= point2.y - 1)
+	{
+		draw_pixel(param, i, j, first_point);
+		j++;
+	}
+}
+
+void	draw_line_up(t_param *param, t_vector point1, t_vector point2, t_vector *first_point)
+{
+	int	dx;
+	int	dz;
+	int	e;
+	int	i;
+	int	j;
+
+//	if (not point 1 and 2 on window)
+//		return ;
+	dx = point2.x - point1.x;
+	dz = point2.z - point1.z;
+	e = dz - dx;
+	i = point1.x;
+	j = point1.z;
+	while (i <= point2.x - 1)
+	{
+		draw_pixel(param, i, j, first_point);
+		if (e >= 0)
+		{
+			j++;
+			e -= dx;
+		}
+		i++;
+		e += dz;
+	}
+}
+
+void	draw_line_up_vert(t_param *param, t_vector point1, t_vector point2, t_vector *first_point)
+{
+	int	dx;
+	int	dz;
+//	int	e;
+	int	i;
+	int	j;
+
+//	if (not point 1 and 2 on window)
+//		return ;
+	dx = point2.x - point1.x;
+	dz = point2.z - point1.z;
+	i = point1.x;
+	j = point1.z;
+	while (j <= point2.z - 1)
+	{
+		draw_pixel(param, i, j, first_point);
+		j++;
 	}
 }
 
@@ -64,11 +134,11 @@ void	draw_map(t_param *param, t_map *map)
 		i = 0;
 		while (i < map->len_x)
 		{
+			point1.x = i * SIZE;
+			point1.y = j * SIZE;
+			point1.z = map->elems[j][i];
 			if (i < map->len_x - 1)
 			{
-				point1.x = i * SIZE;
-				point1.y = j * SIZE;
-				point1.z = map->elems[j][i];
 				point2.x = (i + 1) * SIZE;
 				point2.y = j * SIZE;
 				point2.z = map->elems[j][i + 1];
@@ -76,13 +146,49 @@ void	draw_map(t_param *param, t_map *map)
 			}
 			if (j < map->len_y - 1)
 			{
-				point1.x = i * SIZE;
-				point1.y = j * SIZE;
-				point1.z = map->elems[j][i];
 				point2.x = i * SIZE;
 				point2.y = (j + 1) * SIZE;
 				point2.z = map->elems[j + 1][i];
-				draw_line(param, point1, point2, &first_point);
+				draw_line_vert(param, point1, point2, &first_point);
+			}
+			i++;
+		}
+		j++;
+	}
+}
+
+void	draw_map_up(t_param *param, t_map *map)
+{
+	int			i;
+	int			j;
+	t_vector	point1;
+	t_vector	point2;
+	t_vector	first_point;
+
+	first_point.x = WINDOW_SIZE_W / 2 - map->len_x / 2 * SIZE;
+	first_point.y = WINDOW_SIZE_H / 2 - map->len_y / 2 * SIZE;
+	j = 0;
+	while (j < map->len_y)
+	{
+		i = 0;
+		while (i < map->len_x)
+		{
+			point1.x = i * SIZE;
+			point1.y = j * SIZE;
+			point1.z = map->elems[j][i] * SIZE;
+			if (i < map->len_x - 1)
+			{
+				point2.x = (i + 1) * SIZE;
+				point2.y = j * SIZE;
+				point2.z = map->elems[j][i + 1] * SIZE;
+				draw_line_up(param, point1, point2, &first_point);
+			}
+			if (j < map->len_y - 1)
+			{
+				point2.x = i * SIZE;
+				point2.y = (j + 1) * SIZE;
+				point2.z = map->elems[j + 1][i] * SIZE;
+				draw_line_up_vert(param, point1, point2, &first_point);
 			}
 			i++;
 		}
@@ -94,7 +200,7 @@ void	draw_reload(t_param *param, t_map *map)
 {
 //	param->img = mlx_new_image(param->mlx, WINDOW_SIZE_W + 100, WINDOW_SIZE_H + 100);
 //	param->pixel_img = mlx_get_data_addr(param->img, &(param->bpp), &(param->s_line), &(param->ed));
-	draw_map(param, map);
+	draw_map_up(param, map);
 //	mlx_put_image_to_window(param->mlx, param->wnd, param->img, -50, -50);
 //	mlx_destroy_image(param->mlx, param->img);
 }
